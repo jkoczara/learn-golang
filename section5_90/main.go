525f97ccea0e06db597da05b36a74df5f8211d49
Shakamaru <drednay@gmail.com>
2025-04-27 21:39:39 +0200

    section 6

--- section5_90/main.go
+++ section5_90/main.go
@@ -7,10 +7,32 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
+
+type saver interface {
+	Save() error
+}
+
+type displayer interface {
+	Display()
+}
+
+type outputtable interface {
+	saver
+	displayer
+}
 
 func main() {
 	title, content := getNoteData()
+	todoText := getUserInput("Todo text: ")
+
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	userNote, err := note.New(title, content)
 
@@ -19,15 +41,50 @@ func main() {
 		return
 	}
 
-	userNote.Display()
-	err = userNote.Save()
+	err = outputData(todo)
 
 	if err != nil {
-		fmt.Println("Saving the note failed.")
 		return
 	}
 
-	fmt.Println("Saving the note succedded!")
+	err = outputData(userNote)
+
+	if err != nil {
+		return
+	}
+}
+
+func printSomething(value interface{}) {
+	//typedVal, ok := value.(int)
+
+	// switch value.(type) {
+	// case int:
+	// 	fmt.Println("Integer:", value)
+	// case float64:
+	// 	fmt.Println("Float64:", value)
+	// case string:
+	// 	fmt.Println("String:", value)
+	// default:
+	// 	// ..
+	// }
+	// fmt.Println(value)
+}
+
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
+}
+
+func saveData(data saver) error {
+	err := data.Save()
+
+	if err != nil {
+		fmt.Println("Saving the todo failed.")
+		return err
+	}
+
+	fmt.Println("Saving the todo succedded!")
+	return nil
 }
 
 func getNoteData() (string, string) {
@@ -52,4 +109,8 @@ func getUserInput(prompt string) string {
 	text = strings.TrimSuffix(text, "\r")
 
 	return text
+}
+
+func add[T int | float64 | string](a, b T) T {
+	return a + b
 }
