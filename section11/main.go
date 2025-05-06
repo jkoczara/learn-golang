a1dc283e42d5045fb614298a3d3b2f7b4d6625fe
Shakamaru <drednay@gmail.com>
2025-05-06 22:45:31 +0200

    section11

---
+++ section11/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"example.com/project/db"
+	"example.com/project/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db.InitDB()
+	server := gin.Default()
+	routes.RegisterRoutes(server)
+	server.Run(":8080")
+}
